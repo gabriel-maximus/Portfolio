1b3650b59c1afa14af24b3ca912ea3e90321d97e
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-12-28 02:25:09 -0300

    Novo componente de card com imagem para os projetos

---
+++ src/components/ImageCard/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+
+function ImageCard ({title, text, image, tecnologias, ref}) {
+    return (
+        <div className="card">
+            <img src={image} class="card-img-top" alt="Imagem do Projeto" />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{text}</p>
+                <hr />
+                <strong>Tecnologias Utilizadas: </strong>
+                <p className='tecnologias'>{tecnologias}</p>
+                <div className='botao-card'>
+                    <a href={ref} target={'blank'} className="btn btn-primary">Visitar Projeto</a>
+                </div>    
+            </div>
+        </div>
+    )
+}
+
+export default ImageCard 
