e6f92bbbf86dcad45911a55f9efa3be279bc1a9e
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-12-30 00:33:44 -0300

    Adicionando novo projeto e melhorando responsividade dos image cards

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import ScrollReveal from 'scrollreveal';
 import Header from './components/Header';
 import Button from './components/Button';
 import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io';
-import {DiJavascript1, DiReact} from 'react-icons/di';
-import {SiArduino, SiCplusplus} from 'react-icons/si'
+import {DiJavascript1, DiReact, DiHtml5, DiCss3} from 'react-icons/di';
+import {SiArduino, SiCplusplus, SiBootstrap} from 'react-icons/si'
 import {TbApi} from 'react-icons/tb';
 import Foto from './assets/Foto.png'
 import Card from './components/Card';
 import ImageCard from './components/ImageCard';
 import MaxFlix from './assets/MaxFlix.png';
 import Telemetria from './assets/Telemetria.jpeg'
+import LandingPage from './assets/LandingPage.png'
 
 function App() {
   ScrollReveal({ reset: true });
@@ -48,7 +49,8 @@ function App() {
           tecnologias={<><DiJavascript1 size={20}/><DiReact size={20}/><TbApi size={20} /></>} image={MaxFlix}/>
           <ImageCard title={'Telemetria'} text={'Projeto utilizado para transmissão de dados em tempo real via rádio, com dados de temperatura, pressão e coordenadas geográficas.'}
           tecnologias={<><SiCplusplus size={20}/><SiArduino size={20} /></>} image={Telemetria} redirect={'https://github.com/gabriel-maximus/Avionica'}/>
-          <ImageCard />
+          <ImageCard title={'Landing Page de Seguros'} text={'Landing page fictícia de um site de seguros. Desafio do site Front-End mentor, e que aborda conceitos de responsividade.'}
+          tecnologias={<><DiHtml5 size={20}/><DiCss3 size={20}/><SiBootstrap size={20} /></>} image={LandingPage} redirect={'https://gabriel-maximus.github.io/Landing-Page-Seguros/'}/>
         </div>
       </section>
     </div>
